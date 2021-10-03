3c36520ab79a366eb089f591aff5928e949b4a10
Ivan <gusvidiv@student.gu.se>
2021-10-04 00:09:15 +0200

    Start of review class

---
+++ Store/src/Review.java
@@ -0,0 +1,30 @@
+public class Review {
+    String iD;
+    int grade;
+    String comment;
+
+    //Constructor with optional comment
+    public Review(String iD, int grade, String comment) {
+        this.iD = iD;
+        this.grade = grade;
+        this.comment = comment;
+    }
+
+    //Constructor without optional comment
+    public Review(String iD, int grade) {
+        this.iD = iD;
+        this.grade = grade;
+    }
+
+    public String getiD() {
+        return iD;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+}
